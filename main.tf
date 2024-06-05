97cbf37b10da4e30a3e2d2e80a2d519025cc25a3
LokeshChandu <168253766+LokeshChandu@users.noreply.github.com>
2024-06-05 12:50:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "name" {
+    ami = "ami-00fa32593b478ad6e"
+    instance_type = "t2.micro"
+    key_name = "chandukeypair"
+
+    tags = {
+      Name = "newtag"
+    }
+  
+}
